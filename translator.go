16a4acf7789db3629a9ea96ef73fa8a3ebeb2594
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-10-02 04:21:08 +0530

    Update translator.go

--- translator.go
+++ translator.go
@@ -1,55 +1,80 @@
 package dson
 
+var (
+	quote    bool
+	array    bool
+	escaping bool
+	token    string
+	current  string
+	output   string
+	number   string
+	i        int
+)
+
+func reset() {
+	quote = false
+	array = false
+	escaping = false
+	token = ""
+	current = ""
+	output = ""
+	number = ""
+}
+
+func addSeparator(currentChar string) string {
+	if array {
+		currentChar = getRandom(" and", " also")
+	} else {
+		currentChar = getRandom(".", ",", "!", "?")
+	}
+	return currentChar + " "
+}
+
+func addQuote(currentChar string, isDSON bool) {
+	output += "\"" + current + "\""
+	if !isDSON {
+		output += " "
+	}
+	quote = false
+	current = ""
+}
+
+func checkForArray(currentChar string) {
+	if trimSpace(currentChar) == "" {
+		return
+	}
+	token += currentChar
+	if currentChar == "]" {
+		array = false
+	}
+	if currentChar == "[" {
+		array = true
+	}
+}
+
 func translate(data []rune, keywords map[string]string, isDSON bool) string {
-	var (
-		quote    bool
-		array    bool
-		escaping bool
-		token    string
-		current  string
-		output   string
-		number   string
-	)
-	for i := 0; i < len(data); i++ {
+	reset()
+	for i = 0; i < len(data); i++ {
 		currentChar := string(data[i])
 		if currentChar == "," && !isDSON {
-			if array {
-				currentChar = getRandom(" and", " also")
-			} else {
-				currentChar = getRandom(".", ",", "!", "?")
-			}
-			output = trimSpace(output) + currentChar + " "
+			output = trimSpace(output) + addSeparator(currentChar)
 			continue
 		}
 		if quote {
 			if (currentChar == "\"" || currentChar == "'") && !escaping {
-				output += "\"" + current + "\""
-				if !isDSON {
-					output += " "
-				}
-				quote = false
-				current = ""
+				addQuote(currentChar, isDSON)
 				continue
 			}
 			escaping = currentChar == "\\"
 			current += currentChar
 			continue
 		}
-
 		if currentChar == "\"" {
 			quote = true
 			current = ""
 			continue
 		}
-		if trimSpace(currentChar) != "" {
-			token += currentChar
-			if currentChar == "]" {
-				array = false
-			}
-			if currentChar == "[" {
-				array = true
-			}
-		}
+		checkForArray(currentChar)
 		if isNum(token) || contains(token, "very") {
 			number += token
 			token = ""
@@ -68,7 +93,6 @@ func translate(data []rune, keywords map[string]string, isDSON bool) string {
 		}
 		if lToken := toLower(token); exists(keywords[lToken]) {
 			output += keywords[lToken]
-
 			if !isDSON {
 				output += " "
 			}
